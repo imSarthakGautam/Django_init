66859eeeb300c8a24b9da7cae5af9b4bbb68c5e5
imSarthakGautam <sarthakgtm175@gmail.com>
2024-07-11 21:27:24 +0545

    Basic Structure of Project and Files

--- DjangoFirst/DjangoFirst/views.py
+++ DjangoFirst/DjangoFirst/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello world, You are at DjangoFirst Homepage")
+    #return HttpResponse("Hello world, You are at DjangoFirst Homepage")
+    return render(request, 'website/index.html')
 
 def about(request):
-    return HttpResponse("Hello world, You are at DjangoFirst Aboutpage")
+    #return HttpResponse("Hello world, You are at DjangoFirst Aboutpage")
+    return render(request, 'website/about.html')
 
 def contact(request):
-    return HttpResponse("Hello world, You are at DjangoFirst Contact page")
+    #return HttpResponse("Hello world, You are at DjangoFirst Contact page")
+     return render(request, 'website/contact.html')
